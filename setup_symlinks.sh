8c4e902336cb6fb0fc535dca717a7943e15d5f8c
mickeyh <mickeyh>
2013-05-23 17:10:52 -0700

    Addes symlink setup script to automate the annoying symlinking.

---
+++ setup_symlinks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# NOTE: This script requires for you to have cloned dotfiles into ~/dotfiles.
+
+DOTFILESDIR="./dotfiles"
+
+echo $DOTFILESDIR
+
+# Remove dotfiles so we can later overwrite them.
+rm -i ~/.Xdefaults
+rm -i ~/.Xmodmap
+rm -i ~/.gtkrc-2.0
+rm -rfi ~/.i3
+rm -rfi ~/.vimperator
+rm -i ~/.vimperatorrc
+rm -i ~/.xinitrc
+
+# Make symlinks.
+ln -s $DOTFILESDIR/Xdefaults ~/.Xdefaults
+ln -s $DOTFILESDIR/Xmodmap ~/.Xmodmap
+ln -s $DOTFILESDIR/gtkrc-2.0 ~/.gtkrc-2.0
+ln -s $DOTFILESDIR/i3 ~/.i3
+ln -s $DOTFILESDIR/vimperator ~/.vimperator
+ln -s $DOTFILESDIR/vimperator/vimperatorrc ~/.vimperatorrc
+ln -s $DOTFILESDIR/xinitrc ~/.xinitrc
